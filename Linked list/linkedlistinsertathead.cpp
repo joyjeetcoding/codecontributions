0f0228ddd1796acec70a6e5a9dde8516e544bede
Joyjeet Mukherjee <96014361+joyjeetcoding@users.noreply.github.com>
2022-08-01 00:32:58 +0530

    Code uploaded on 01/08/2022

---
+++ Linked list/linkedlistinsertathead.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    // constructor
+    Node(int data)
+    {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void insertathead(Node* &head, int d)
+{
+    //create new node
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void print(Node* &head)
+{
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node* node1 = new Node(10);
+    // cout << node1 -> data << endl;
+    // cout << node1 -> next << endl;
+
+
+    //head pointed to node1
+    Node* head = node1;
+    print(head);
+
+    insertathead(head, 12);
+
+    print(head);
+
+    insertathead(head, 15);
+
+    print(head);
+
+    return 0;
+}
