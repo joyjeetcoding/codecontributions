9cd01df2f679e663148b22d0c640bd550e24fde1
Joyjeet Mukherjee <96014361+joyjeetcoding@users.noreply.github.com>
2022-08-22 01:25:53 +0530

    Update Linked List acc to user input.c

--- Linked list/Linked List acc to user input.c
+++ Linked list/Linked List acc to user input.c
@@ -42,27 +42,81 @@ void print(struct Node *head)
     printf("\n");
 }
 
-struct Node* reverse(struct Node* head)
+struct Node *reverse(struct Node *head)
 {
-    struct Node* prev = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* curr = head;
+    struct Node *prev = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *curr = head;
 
     prev = NULL;
 
-    while(curr != NULL)
+    while (curr != NULL)
     {
-        struct Node* forward = curr -> next;
-        curr -> next = prev;
+        struct Node *forward = curr->next;
+        curr->next = prev;
         prev = curr;
         curr = forward;
     }
     return prev;
 }
 
+struct Node *solve(struct Node *head, struct Node *head2)
+{
+    // if the first node contains single element then connect the 1st node to the 2nd one
+    if (head->next == NULL)
+    {
+        head->next = head2;
+    }
+
+    struct Node *curr1 = head;
+    struct Node *next1 = curr1->next;
+    struct Node *curr2 = head2;
+    struct Node *next2 = curr2->next;
+
+    while (next1 != NULL && curr2 != NULL)
+    {
+        if ((curr1->data <= curr2->data) && (curr2->data <= next1->data))
+        {
+            curr1->next = curr2;
+            next2 = curr2->next;
+            curr2->next = next1;
+
+            curr1 = curr2;
+            curr2 = next2;
+        }
+
+        else
+        {
+            curr1 = next1;
+            next1 = next1->next;
+            if (next1 == NULL)
+            {
+                curr1->next = curr2;
+                return head;
+            }
+        }
+    }
+    return head;
+}
+
+struct Node *merge(struct Node *head, struct Node *head2)
+{
+    if (head == NULL)
+        return head2;
+    if (head2 == NULL)
+        return head;
+
+    if (head->data <= head2->data)
+        return solve(head, head2);
+    else
+        return solve(head2, head);
+}
+
 int main()
 {
     struct Node *node1 = (struct Node *)malloc(sizeof(struct Node));
     struct Node *head = node1;
+    struct Node *head2 = node1;
+
     int ch;
 
     do
@@ -70,7 +124,7 @@ int main()
         printf("\n");
         printf("1. Create a Linked List\n");
         printf("2. Reverse the Linked List\n");
-        printf("3. Merge two Linked Lists\n");
+        printf("3. Merge two Sorted Linked Lists\n");
         printf("4. Exit\n");
         printf("Enter you choice:\n");
         scanf("%d", &ch);
@@ -86,8 +140,12 @@ int main()
             print(head);
             break;
         case 3:
-            head = create();
-            // merge(head);
+            head2 = create();
+            merge(head, head2);
+            print(head);
+            break;
+        case 4:
+            exit(0);
             break;
         default:
             printf("Wrong Choice\n");
